19913499c182f7a37b3849e14a76119383e57619
David Frank <bitinn@gmail.com>
2015-03-31 16:32:47 +0800

    basic oauth added

--- components/config/default.js
+++ components/config/default.js
@@ -25,4 +25,22 @@ module.exports = {
 		, pass: ''
 		, database: 0
 	}
+	, oauth: {
+		server: {
+			protocol: 'http'
+			, host: '127.0.0.1:8081'
+			, callback: '/callback'
+		}
+		, twitter: {
+			key: 'nxOx6Y7lpmN4e4V99L0bw'
+			, secret: '4nZX1am59WcgB0PAy0LYDCHdXytEDZOmGWvMoiWtk'
+		}
+		, github: {
+			key: 'a6fea3770028d383d712'
+			, secret: '3e1bfca8ffdbd7155fed6964e71281266b7eeafb'
+		}
+	}
+	, cookies: {
+		key: '2($<`)te+(CX,lm5LtRg6V3}c<|938_vO>%|OD~|2$?<6 -s-!p!ZVj0#.Ib<bon'
+	}
 };
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 var koa = require('koa');
 var logger = require('koa-logger');
+var mount = require('koa-mount');
+var session = require('koa-session');
+var bodyParser = require('koa-bodyparser');
 
 var configFactory = require('./components/config/config');
 var config = configFactory();
@@ -9,7 +12,11 @@ var db = require('./components/db/db');
 var dev = require('./components/dev/dev');
 var router = require('./components/router/router');
 
+var Grant = require('grant-koa');
+var grant = new Grant(config.oauth);
+
 var app = koa();
+app.keys = [config.cookies.key];
 
 app.use(logger());
 app.use(dev(app.env));
@@ -17,4 +24,8 @@ app.use(configFactory(true));
 app.use(db());
 
 router(app);
+app.use(mount(grant));
+app.use(bodyParser());
+app.use(session(app));
+
 app.listen(config.server.port);
