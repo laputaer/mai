1d4cad199d7ab62b5bae446b6d3eb72ea5ce7ad7
David Frank <bitinn@gmail.com>
2015-04-27 00:05:17 +0800

    update roadmap

---
+++ components/security/transform.js
@@ -0,0 +1,19 @@
+
+/**
+ * transform.js
+ *
+ * A helper function for data transform
+ */
+
+module.exports = transform;
+
+/**
+ * Transform input based on defined transform rules
+ *
+ * @param   Object  data  Object to transform
+ * @param   String  name  Transform name
+ * @return  Object
+ */
+function transform(data, name) {
+
+};
