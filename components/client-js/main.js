148a5c1bb811205cb3b3c03b631a1fdc64352a12
David Frank <bitinn@gmail.com>
2015-05-05 01:06:27 +0800

    simple render loop

--- components/client-js/main.js
+++ components/client-js/main.js
@@ -1,2 +1,21 @@
 
+/**
+ * main.js
+ *
+ * Client-side app
+ */
+
 'use strict';
+
+var raf = require('raf');
+
+var patcher = require('./view/patcher');
+
+var renderer = function() {
+	return '<h1>' + Date.now() + '</h1>';
+};
+
+raf(function loop() {
+	patcher(document.body, renderer());
+	raf(loop);
+});
---
+++ components/client-js/view/patcher.js
@@ -0,0 +1,64 @@
+
+/**
+ * patcher.js
+ *
+ * Take HTML as string, convert into virtual dom and cache, update as HTML change
+ */
+
+'use strict';
+
+// vdom to html
+var diff = require('virtual-dom/diff');
+var patch = require('virtual-dom/patch');
+var createElement = require('virtual-dom/create-element');
+
+// html to vdom
+var VNode = require('virtual-dom/vnode/vnode');
+var VText = require('virtual-dom/vnode/vtext');
+var h2v = require('html-to-vdom')({
+	VNode: VNode
+	, VText: VText
+});
+
+var htmlCache, vdomCache, nodeCache;
+
+module.exports = patcher;
+
+/**
+ * Patch HTML using virtual-dom
+ *
+ * @param   DOM     base  Container dom
+ * @param   String  html  Input HTML string
+ * @return  Void
+ */
+function patcher(base, html) {
+	// STEP 1: same html string, skip rendering
+	if (htmlCache === html) {
+		return;
+	}
+
+	// STEP 2: cache html
+	htmlCache = html;
+
+	// STEP 3: convert html into vdom
+	var vdom = h2v(html); 
+
+	// STEP 4: first render
+	if (!vdomCache || !nodeCache) {
+		// update cache
+		vdomCache = vdom;
+		nodeCache = createElement(vdom);
+		// clean up container
+		while (base.firstChild) {
+			base.removeChild(base.firstChild);
+		}
+		// add rendered dom
+		base.appendChild(nodeCache);
+		return;
+	}
+
+	// STEP 5: update view
+	var patches = diff(vdomCache, vdom);
+	patch(nodeCache, patches);
+	vdomCache = vdom;
+};
