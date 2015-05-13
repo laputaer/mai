83cf4fe45687bcec632810cb5663cc7faed68ff2
David Frank <bitinn@gmail.com>
2015-05-13 22:50:56 +0800

    update error page

--- components/templates/common/custom-error.js
+++ components/templates/common/custom-error.js
@@ -17,9 +17,30 @@ module.exports = template;
  * @return  VNode
  */
 function template(data) {
-	var error = h('div.error', [
-		h('p.line', data.error_status)
-		, h('p.line', data.error_message)
+	var i18n = data.i18n;
+	var error = h('div.m-section', [
+		h('p.m-subtitle', data.error_status)
+		, h('p.m-line', data.error_message)
+		, h('p.m-line', i18n.t('placeholder.suggestion'))
+		, h('p.m-line', [
+			h('span', i18n.t('placeholder.main-feedback'))
+			, h('a.m-link', {
+				href: 'https://twitter.com/bitinn'
+				, target: '_blank'
+			}, [
+				h('span.m-text.en', '@bitinn')
+			])
+		])
+		, h('p.m-line', [
+			h('span', i18n.t('placeholder.secondary-feedback'))
+			, h('a.m-link', {
+				href: 'https://github.com/maihq/feedbacks'
+				, target: '_blank'
+			}, [
+				h('span.m-text', i18n.t('placeholder.secondary-feedback-name'))
+			])
+			, h('span', i18n.t('placeholder.secondary-feedback-action'))
+		])
 	]);
 
 	return error;
