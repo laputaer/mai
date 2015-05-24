a41822a6fddf5cb0e686dc3cc7769a1ba3536936
David Frank <bitinn@gmail.com>
2015-05-25 01:08:28 +0800

    revert request limit, we should resolve dns issue

--- components/config/default.js
+++ components/config/default.js
@@ -38,7 +38,7 @@ module.exports = {
 	, request: {
 		user_agent: 'Mai/1.0 (+http://mai3.me/)'
 		, follow: 5
-		, timeout: 1000 * 15
+		, timeout: 1000 * 10
 		, size: 1000 * 1000 * 10
 	}
 	, session: {
