8783a856402be2e13408a0ae08f38fa2664e162c
David Frank <bitinn@gmail.com>
2015-07-15 19:39:10 +0800

    fix image proxy and bump asset version

--- components/config/default.js
+++ components/config/default.js
@@ -46,9 +46,9 @@ module.exports = {
 		, maxAge: 1000 * 60 * 60 * 24 * 30
 	}
 	, version: {
-		css: 'v1.0.18'
-		, js: 'v1.0.18'
-		, asset: 'v1.0.18'
+		css: 'v1.0.19'
+		, js: 'v1.0.19'
+		, asset: 'v1.0.19'
 	}
 	, output: {
 		format: false
