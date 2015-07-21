8f6a34bbc86346d92f8b8191bf163cfb51760963
David Frank <bitinn@gmail.com>
2015-07-22 02:29:21 +0800

    migration script update

---
+++ migration/memberships-v2.js
@@ -0,0 +1,40 @@
+
+/**
+ * memberships-v1.js
+ *
+ * Migration script for `memberships` collection, rev.2
+ */
+
+var checkRev = require('./check-revision');
+var updateRev = require('./update-revision');
+
+module.exports = migration;
+
+function *migration(db) {
+	var opts = {
+		revision: 2
+		, name: 'memberships'
+	};
+
+	// check revision
+	var status = yield checkRev(db, opts);
+	if (!status) {
+		return;
+	}
+
+	// run migration
+	var Target = db.col(opts.name);
+
+	yield Target.index({
+		uid: 1
+		, created: -1
+	});
+
+	yield Target.index({
+		slug: 1
+		, created: -1
+	});
+
+	// update revision
+	yield updateRev(db, opts);
+};
---
+++ migration/posts-v3.js
@@ -0,0 +1,45 @@
+
+/**
+ * posts-v1.js
+ *
+ * Migration script for `posts` collection, rev.3
+ */
+
+var checkRev = require('./check-revision');
+var updateRev = require('./update-revision');
+
+module.exports = migration;
+
+function *migration(db) {
+	var opts = {
+		revision: 3
+		, name: 'posts'
+	};
+
+	// check revision
+	var status = yield checkRev(db, opts);
+	if (!status) {
+		return;
+	}
+
+	// run migration
+	var Target = db.col(opts.name);
+
+	// TODO: drop old indexes
+	yield Target.index({
+		club: 1
+		, created: -1
+	});
+
+	yield Target.index({
+		user: 1
+		, created: -1
+	});
+
+	yield Target.index({
+		created: -1
+	});
+
+	// update revision
+	yield updateRev(db, opts);
+};
