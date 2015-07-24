fb53a0615f2a2d945a1a067d9a54a5c9f7e3308e
David Frank <bitinn@gmail.com>
2015-07-24 18:50:00 +0800

    new footer design

--- components/config/default.js
+++ components/config/default.js
@@ -46,9 +46,9 @@ module.exports = {
 		, maxAge: 1000 * 60 * 60 * 24 * 30
 	}
 	, version: {
-		css: 'r2015072402'
-		, js: 'r2015072402'
-		, asset: 'r2015072402'
+		css: 'r2015072403'
+		, js: 'r2015072403'
+		, asset: 'r2015072403'
 	}
 	, output: {
 		format: false
