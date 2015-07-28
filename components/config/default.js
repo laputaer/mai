d4ace3eec7054ca6c60f7ffd1da383ba60e99cd0
David Frank <bitinn@gmail.com>
2015-07-28 23:40:50 +0800

    new build for testing

--- components/config/default.js
+++ components/config/default.js
@@ -46,9 +46,9 @@ module.exports = {
 		, maxAge: 1000 * 60 * 60 * 24 * 30
 	}
 	, version: {
-		css: 'r2015072801'
-		, js: 'r2015072801'
-		, asset: 'r2015072801'
+		css: 'r2015072802'
+		, js: 'r2015072802'
+		, asset: 'r2015072802'
 	}
 	, output: {
 		format: false
